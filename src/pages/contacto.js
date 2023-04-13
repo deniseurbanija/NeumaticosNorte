67d8dd26c273cae6aa1902633321fd0fe47298f7
Denise Urbanija <you@example.com>
2023-04-13 01:06:30 -0300

    Link

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -5,24 +5,26 @@ export default function Contacto() {
     <>
       <div className={styles.div}>
         <h1>Contacto</h1>
-        <p>
-          <span>Direccion: </span> Av. 25 de mayo 1170 - Resistencia, Chaco
-        </p>
-        <p>
-          <span>Telefono: </span> (0362) 442-9900
-        </p>
-        <p>
-          <span>Celular: </span> (0362) 421-3700
-        </p>
-        <p>
-          <span>Email: </span> neumaticosnortesrl@hotmail.com
-        </p>
-        <p>
-          <span>Facebook: </span> Neumaticos Norte
-        </p>
-        <p>
-          <span>Instagram: </span> @neumaticosnorte
-        </p>
+        <div className={styles.texto}>
+          <p>
+            <span>Direccion: </span> Av. 25 de mayo 1170 - Resistencia, Chaco
+          </p>
+          <p>
+            <span>Telefono: </span> (0362) 442-9900
+          </p>
+          <p>
+            <span>Celular: </span> (0362) 421-3700
+          </p>
+          <p>
+            <span>Email: </span> neumaticosnortesrl@hotmail.com
+          </p>
+          <p>
+            <span>Facebook: </span> Neumaticos Norte
+          </p>
+          <p>
+            <span>Instagram: </span> @neumaticosnorte
+          </p>
+        </div>
       </div>
     </>
   );
