84c26812ae76fdab64f8d70698101d166d1afe5b
deniseurbanija <deniseurbanija1@gmail.com>
2025-01-03 12:17:13 -0300

    styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,27 +4,19 @@ import styles from "@/styles/Card.module.css";
 export default function Card() {
   return (
     <>
-      <Link href="autos" className={styles.div}>
-        <div>
-          <img className={styles.img} src="img/autos.png"></img>
-          <h2 className={styles.title}>Autos y camionetas</h2>
-          <p>Lorem ipsum dolor sit</p>
-        </div>
-      </Link>
-      <Link href="camiones" className={styles.div}>
-        <div>
-          <img className={styles.img} src="img/camiones.png"></img>
-          <h2 className={styles.title}>Camiones</h2>
-          <p>Lorem ipsum dolor sit</p>
-        </div>
-      </Link>
-      <Link href="agricola" className={styles.div}>
-        <div>
-          <img className={styles.img} src="img/agricola.png"></img>
-          <h2 className={styles.title}>Agrícola</h2>
-          <p>Lorem ipsum dolor sit</p>
-        </div>
-      </Link>
+      <div className={styles.div}>
+        <img className={styles.img} src="img/autos.png"></img>
+        <h2 className={styles.title}>Autos y camionetas</h2>
+      </div>
+      <div className={styles.div}>
+        <img className={styles.img} src="img/camiones.png"></img>
+        <h2 className={styles.title}>Camiones</h2>
+      </div>
+
+      <div className={styles.div}>
+        <img className={styles.img} src="img/agricola.png"></img>
+        <h2 className={styles.title}>Agrícola</h2>
+      </div>
     </>
   );
 }
--- src/pages/nosotros.js
+++ src/pages/nosotros.js
@@ -5,13 +5,30 @@ export default function SobreNosotros() {
     <div className={styles.div}>
       <h1>Sobre Nosotros</h1>
       <p className={styles.texto}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
+        En Neumáticos Norte, somos líderes en la región brindando soluciones
+        integrales en neumáticos, servicios automotrices y productos
+        relacionados. Desde nuestros inicios, nos hemos destacado por ofrecer
+        calidad, confianza y un servicio profesional adaptado a las necesidades
+        de nuestros clientes.
+      </p>
+      <h2>Nuestra Red de Sucursales</h2>
+      <p className={styles.texto}>
+        Casa Central - Posadas, Misiones Ubicados en Ruta 12, N°8012, Km 5 (Tel:
+        0376-4481555), contamos con una moderna planta de recapados Bandag para
+        neumáticos de camiones, reconocida por Bridgestone/Bandag por su
+        excelencia. Servicios: Autos y utilitarios: alineación, balanceo, tren
+        delantero, amortiguación y reparación de llantas. Camiones: colocación y
+        balanceo computarizado. Productos: Venta de neumáticos, llantas, cámaras
+        y baterías para autos, utilitarios, camiones, vehículos agrícolas y
+        viales.
+        <br />
+        Sucursal Resistencia, Chaco Ubicada en Av. 25 de Mayo 1170 (Tel:
+        0362-4429900). Servicios: Alineación, balanceo, tren delantero,
+        amortiguación, frenos y reparación de llantas para autos y pick-ups.
+        Balanceo para motos (únicos en la zona). Productos: Venta de neumáticos
+        para motos, autos, pick-ups, camiones, agrícolas y viales. También
+        ofrecemos baterías, llantas deportivas y originales. Logística: Enviamos
+        neumáticos a recapar a nuestra casa central sin costo de envío.
       </p>
     </div>
   );
